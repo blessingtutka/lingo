1a371b687175ad55941fa822b3e2f058efbea656
blessingtutka <blessingtutka298@gmail.com>
2025-06-29 16:25:52 +0200

    chore: add App

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import UserProvider from './providers/user.provider';
+import { SocketProvider } from './providers/socket.provider';
+import { Ringtone } from './components/global';
+import routes from './routes';
+import './app.css';
+
+const router = createBrowserRouter(routes);
+
+const App = () => {
+    return (
+        <UserProvider>
+            <SocketProvider>
+                <Ringtone />
+                <RouterProvider router={router} />
+            </SocketProvider>
+        </UserProvider>
+    );
+};
+
+export default App;
