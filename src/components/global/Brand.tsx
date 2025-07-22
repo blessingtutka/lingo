ad7270d006dd30fe6523a7c98c6a6d45a45379a8
blessingtutka <blessingtutka298@gmail.com>
2025-07-22 16:47:32 +0200

    refactor(global): update global compoments

--- src/components/global/Brand.tsx
+++ src/components/global/Brand.tsx
@@ -2,7 +2,7 @@ import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 export function Brand() {
     return (
-        <Link to='/' className=' flex items-center gap-1.5'>
+        <Link to='/user' className=' flex items-center gap-1.5'>
             <img src={logo} className='!size-8' />
             <span className='text-base font-semibold'>Lingo</span>
         </Link>
--- src/components/global/UserAvatar.tsx
+++ src/components/global/UserAvatar.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 
-export const UserAvatar = ({ user }: { user: User }) => (
-    <Avatar>
+export const UserAvatar = ({ user, className }: { user: User; className?: string }) => (
+    <Avatar className={`${className}`}>
         {user.avatar ? <AvatarImage src={user.avatar} alt={user.name || user.email} /> : null}
         <AvatarFallback className='bg-blue-400'>{(user.name || user.email).substring(0, 2).toUpperCase()}</AvatarFallback>
     </Avatar>
