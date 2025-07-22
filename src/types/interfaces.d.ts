b6b2fbe65c8471eeece66c56ba49a628cd0b4e1b
blessingtutka <blessingtutka298@gmail.com>
2025-07-22 16:48:45 +0200

    refactor(type): update intefaces

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -35,3 +35,26 @@ interface ApiResponse<T> {
     data: T;
     status_code: number;
 }
+
+type CallStatus = 'REQUESTING' | 'ONGOING' | 'MISSED' | 'REJECTED' | 'ENDED';
+
+interface Call {
+    id: string;
+    startedAt: string;
+    endedAt: string | null;
+    status: CallStatus;
+    peerId?: string;
+    callerId: string;
+    receiverId: string;
+    isCaller?: boolean;
+    summary?: {
+        id: string;
+        content: string;
+    };
+}
+interface ApiResponse<T> {
+    status: string;
+    message: string;
+    data: T;
+    status_code: number;
+}
