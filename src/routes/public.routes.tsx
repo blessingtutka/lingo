5a4a22e4b677fe623cf2f381ec85cd17ea4a2f55
blessingtutka <blessingtutka298@gmail.com>
2025-07-29 22:27:30 +0200

    perf(routes): update account routes

--- src/routes/public.routes.tsx
+++ src/routes/public.routes.tsx
@@ -1,8 +1,14 @@
 import Home from '@/pages/Home';
 import { AuthTabs } from '@/pages/Auth';
+import { ResetPassword } from '@/components/account/ResetPassword';
+import { PasswordResetRequest } from '@/components/account/PasswordResetRequest';
+import { EmailVerifed } from '@/components/account/EmailVerified';
 const publicRoutes = [
     { path: '', element: <Home />, exact: true },
     { path: 'auth', element: <AuthTabs /> },
+    { path: '/password-reset-request', element: <PasswordResetRequest /> },
+    { path: '/password-reset', element: <ResetPassword /> },
+    { path: '/verify-email', element: <EmailVerifed /> },
 ];
 
 export default publicRoutes;
